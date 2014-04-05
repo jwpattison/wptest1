e7941ebc8409ccf0bcd42df9b86fe2798b930db0
Joel Pattison <joelpattison@gmail.com>
2014-04-05 10:50:46 -0400

    Removed some title text

--- page.php
+++ page.php
@@ -5,11 +5,7 @@
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages and that
  * other 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
+
 
 get_header(); ?>
 
